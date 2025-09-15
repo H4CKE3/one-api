297fcba5517aa1930c3200db2f73489bb07fce92
RE <2350299706@qq.com>
2025-09-15 16:39:45 +0800

    update

--- requestion.py
+++ requestion.py
@@ -5,8 +5,8 @@ response = requests.post(
     "http://localhost:3000/v1/chat/completions",
     headers={"Authorization": "Bearer sk-cFlmQUgFBMyoPR6z7c70Ad22108640568c64D75eFaBf64B3"},
     json={
-        # "model": "gemini-1.5-flash",
-        "model": "deepseek-chat",
+        "model": "gemini-1.5-flash",
+        # "model": "deepseek-chat",
         "messages": [{"role": "user", "content": "你是什么大模型，介绍一下自己"}]
     }
 )
